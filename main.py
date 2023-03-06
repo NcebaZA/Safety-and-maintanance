8ad896928ecbc31a961c274916aee9bcef7dc840
Phiwa Dludlu <phiwadludlu79@gmail.com>
2023-03-06 21:42:20 +0200

    Flask basic layout

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://snmuk0n4ibb90h3tn0qm:pscale_pw_PDs9RvKc4IHBtwg0Vv3AWtvKlOkkNrnRusUx5WeodsYd@maintenance/db"
+app.config['SQLALCHEMY_TRACL_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
